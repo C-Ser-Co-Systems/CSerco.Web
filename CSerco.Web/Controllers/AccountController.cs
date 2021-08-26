8016e07221bebce2cad557506c766263a5e0ab8f
Rodrigo Jose Mejia Aguilar <Santii_mejia@live.com>
2021-08-26 12:25:26 -0600

    Se agrega la carpeta de imagenes, se completa diseño de formulario de Iniciar Sesion, se generan Scripts de Style y JS para funcionamiento de login, se agrega Clases de manejo de servicios de usuario

--- CSerco.Web/Controllers/AccountController.cs
+++ CSerco.Web/Controllers/AccountController.cs
@@ -3,15 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CSerco.Web.Models;
+using CSerco.Web.Services;
 
 namespace CSerco.Web.Controllers
 {
     public class AccountController : Controller
     {
+        readonly UsersRepository _manejoUserServices = new UsersRepository();
         // GET: Account
+        public ActionResult LogIn()
+        {
+            return View();
+        }
+
         public ActionResult NewUser()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult NewUser(UserVM model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            else
+            {
+                if (_manejoUserServices.AddNewUser(model))
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ViewBag.Message = "BAD";
+                    return View(model);
+                }
+            }
         }
     }
 }
---
+++ CSerco.Web/Services/UsersRepository.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using System.ComponentModel;
+using System.Security.Cryptography;
+using CSerco.SQL.DataContext;
+using CSerco.Web.Models;
+using System.Diagnostics;
+
+namespace CSerco.Web.Services
+{
+    public class UsersRepository
+    {
+        private readonly CSercoDBEntities1 db = new CSercoDBEntities1();
+
+        public bool AddNewUser(UserVM model)
+        {
+            try
+            {
+                Usuarios user = new Usuarios
+                {
+                    IdRol = model.IdRol,
+                    UserName = model.UserName,
+                    PasswordHash = Encrypt(model.Password),
+                    FechaRegistro = DateTime.Today,
+                    Status = 1
+                };
+                db.Usuarios.Add(user);
+                db.SaveChanges();
+                //It´s all good!
+                return true;
+            }catch(Exception e)
+            {
+                Debug.WriteLine("Exception Message: " + e.Message);
+                return false;
+            }
+        }
+
+        public String Encrypt(string pass)
+        {
+            byte[] salt;
+            byte[] bytes;
+            if (pass == null)
+            {
+                pass = "DummyPass123";
+            }
+            using (Rfc2898DeriveBytes rfc2898DeriveByte = new Rfc2898DeriveBytes(pass, 16, 1000))
+            {
+                salt = rfc2898DeriveByte.Salt;
+                bytes = rfc2898DeriveByte.GetBytes(32);
+            }
+            byte[] numArray = new byte[49];
+            Buffer.BlockCopy(salt, 0, numArray, 1, 16);
+            Buffer.BlockCopy(bytes, 0, numArray, 17, 32);
+            return Convert.ToBase64String(numArray);
+        }
+    }
+}
