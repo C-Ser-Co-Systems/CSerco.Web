3d0c52ea80156ffaf62b42e0be668558e6c3df3e
Jose System <Jose System@DESKTOP-4R2GSAE>
2021-08-26 09:32:32 -0600

    Agregar archivos de proyecto.

---
+++ CSerco.Web/Filters/Access.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CSerco.Web.Filters
+{
+    public class Access : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            var user = HttpContext.Current.Session["User"];
+            var userRol = HttpContext.Current.Session["Rol"].ToString();
+
+            if(user == null)
+            {
+                filterContext.Result = new RedirectResult("~/Account/Login");
+            }else if(user != null && userRol == "3")
+            {
+                filterContext.Result = new RedirectResult("~/Account/Login");
+            }
+            base.OnActionExecuted(filterContext);
+        }
+    }
+}
---
+++ CSerco.Web/Models/UserVM.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CSerco.Web.Models
+{
+    public class UserVM
+    {
+        public int IdUser { get; set; }
+        [Required(ErrorMessage ="Debe definir un rol para el usuario!")]
+        public int IdRol { get; set; }
+        [Required(ErrorMessage = "El nombre de usuario es requerido!")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "La contraseña es requerida!")]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Debe confirmar la contraseña!")]
+        [Compare("Password", ErrorMessage = "Las contraseñas no coinciden!")]
+        public string ConfirmPassword { get; set; }
+        public DateTime fechaRegistro { get; set; }
+        public DateTime fLastUpdate { get; set; }
+
+        public List<UserVM> UserLst { get; set; }
+
+    }
+}
